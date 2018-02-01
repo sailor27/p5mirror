12950140d72dbbb2c5763baa993d31ff6a903c7b
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-31 16:50:57 -0800

    Load pixels and loop through pixels using nested for loop. Update pixels in loop to change rgba values based on position and using random for green.

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ var video;
 
 function setup() {
   createCanvas(320, 240);
+  pixelDensity(1);
 
   video = createCapture(VIDEO);
 }
@@ -12,10 +13,15 @@ function draw() {
 
   loadPixels();
 
-  pixels[0] = 255;
-  pixels[1] = 255;
-  pixels[2] = 255;
-  pixels[3] = 255;
+  for (var y = 0; y < height; y++) {
+    for (var x = 0; x < width; x++) {
+      var index = (x + y * width)*4;
+      pixels[index + 0] = x;
+      pixels[index + 1] = random(255);
+      pixels[index + 2] = y;
+      pixels[index + 3] = 255;
+    }
+  }
 
 
   updatePixels();
